ef902d888ba7486bbf02f37fd3a79c800b6c7557
tateisu <tateisu@gmail.com>
2017-05-24 14:06:38 +0900

    不具合調査

--- mastodon-fcm-sender.js
+++ mastodon-fcm-sender.js
@@ -275,6 +275,7 @@ app.post('/register', (req, res) => {
         .then( (model,created) => {
 
         if (model ) {
+            model = model[0];
             npmlog.log('info',"register: " +model)
             npmlog.log('info',"register: " +util.inspect(model) )
             npmlog.log('info',"register: " +model.set)
