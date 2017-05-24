f7a53e6d704d0e8250d7b0471efb092e0f79363c
tateisu <tateisu@gmail.com>
2017-05-24 09:21:00 +0900

    add /counter API

--- mastodon-fcm-sender.js
+++ mastodon-fcm-sender.js
@@ -370,14 +370,14 @@ app.get('/counter', (req, res) => {
     var count;
     
     counter_db.serialize(function() {
-        counter_db.run('create table if not exists counter( id integer primary key AUTOINCREMENT)');
+        counter_db.run('create table if not exists counter( id integer primary key AUTOINCREMENT,a text)');
         counter_db.run("BEGIN TRANSACTION");
-        counter_db.run('insert into counter() values ()', { } );
+        counter_db.run('insert into counter(a) values ($a)', {$a: '' } );
         counter_db.get('select max(id) as a from counter',{}, function (err, res) {
             count = res.a;
         });
-        counter_db.run('insert into counter() values ()', { } );
-        counter_db.run("END TRANSACTION");
+        counter_db.run('delete from counter where id < $a',{$a : count});
+        counter_db.run("COMMIT");
     });
     
     res.status(200).send(count);
