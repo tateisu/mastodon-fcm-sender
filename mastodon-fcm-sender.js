db711d917815a83e8e1a2cf6e04abc04dbb0865a
tateisu <tateisu@gmail.com>
2017-05-24 16:30:23 +0900

    DB_PORTを指定可能にした

--- mastodon-fcm-sender.js
+++ mastodon-fcm-sender.js
@@ -26,8 +26,9 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASS,
     {
-        host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
         logging: npmlog.verbose
         //    storage: 'db/fcm-sender.sqlite'
     }
