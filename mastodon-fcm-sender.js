c6c92d8a67f76dfd800316af2f9aa2d8a9809293
tateisu <tateisu@gmail.com>
2017-05-24 14:14:12 +0900

    不具合調査

--- mastodon-fcm-sender.js
+++ mastodon-fcm-sender.js
@@ -9,13 +9,13 @@ import fs from 'fs'
 import util from 'util'
 
 
-const app       = express()
-const port      = process.env.PORT || 4001
+const app = express()
+const port = process.env.PORT || 4001
 
 const callbackUrl = process.env.CALLBACK_URL;
 
 const sequelize = new Sequelize('sqlite://fcm-sender.sqlite', {
-  logging: npmlog.verbose,
+    logging: npmlog.verbose,
     storage: 'db/fcm-sender.sqlite'
 })
 
@@ -23,7 +23,7 @@ const appMap = Hjson.parse(fs.readFileSync('db/app_map.hjson', 'utf8'));
 
 const instanceMap = Hjson.parse(fs.readFileSync('db/instance_map.hjson', 'utf8'));
 
-const checkAppId = (appId ) => {
+const checkAppId = (appId) => {
     if (!appId) {
         return 'missing app_id';
     }
@@ -32,10 +32,10 @@ const checkAppId = (appId ) => {
         return 'missing app configuration for app: ' + appId
     }
     var serverKey = appEntry.serverKey;
-    if(! serverKey ){
+    if (!serverKey) {
         return 'missing server key in app configuration : ' + appId
     }
-    
+
     return null;
 }
 
@@ -50,47 +50,47 @@ const getServerKey = (appId) => {
     return null;
 }
 
-const getInstanceEntry = ( instanceUrl) =>{
-    var instanceEntry = instanceMap[ instanceUrl ];
-    if( ! instanceEntry ){
-        instanceEntry = instanceMap[ '*' ];
+const getInstanceEntry = (instanceUrl) => {
+    var instanceEntry = instanceMap[instanceUrl];
+    if (!instanceEntry) {
+        instanceEntry = instanceMap['*'];
     }
     return instanceEntry;
 }
 
-const getAppSecret = (instanceEntry,appId) =>{
+const getAppSecret = (instanceEntry, appId) => {
     var apps = instanceEntry.apps;
-    if( apps ){
-        return apps[ appId ];
+    if (apps) {
+        return apps[appId];
     }
     return null;
 }
 
-const checkInstanceUrl = (instanceUrl,appId) =>{
-    if( ! instanceUrl ){
+const checkInstanceUrl = (instanceUrl, appId) => {
+    if (!instanceUrl) {
         return 'missing instance_url';
     }
-    
+
     var instanceEntry = getInstanceEntry(instanceUrl);
-    if( ! instanceEntry ){
-        return 'missing instance configuration for instance: ' + instanceUrl ;
+    if (!instanceEntry) {
+        return 'missing instance configuration for instance: ' + instanceUrl;
     }
-    
-    if(! instanceEntry.urlStreamingListenerRegister ){
-        return 'missing urlStreamingListenerRegister configuration: ' + instanceUrl ;
+
+    if (!instanceEntry.urlStreamingListenerRegister) {
+        return 'missing urlStreamingListenerRegister configuration: ' + instanceUrl;
     }
-    if(! instanceEntry.urlStreamingListenerUnregister ){
-        return 'missing urlStreamingListenerUnregister configuration: ' + instanceUrl ;
+    if (!instanceEntry.urlStreamingListenerUnregister) {
+        return 'missing urlStreamingListenerUnregister configuration: ' + instanceUrl;
     }
     const apps = instanceEntry.apps;
-    if(!apps){
-        return 'missing apps configuration: ' + instanceUrl ;
+    if (!apps) {
+        return 'missing apps configuration: ' + instanceUrl;
     }
-    const appSecret = apps[ appId ];
-    if(!appSecret){
-        return 'missing appSecret for app=' + appId + ', instance='+ instanceUrl ;
+    const appSecret = apps[appId];
+    if (!appSecret) {
+        return 'missing appSecret for app=' + appId + ', instance=' + instanceUrl;
     }
-    
+
     return null;
 }
 
@@ -101,7 +101,7 @@ const Registration = sequelize.define('registration', {
         type: Sequelize.BIGINT,
         defaultValue: 0
     },
-    
+
     instanceUrl: {
         type: Sequelize.STRING
     },
@@ -117,7 +117,7 @@ const Registration = sequelize.define('registration', {
     accessToken: {
         type: Sequelize.STRING
     },
-    
+
     deviceToken: {
         type: Sequelize.STRING
     }
@@ -126,16 +126,16 @@ const Registration = sequelize.define('registration', {
 
 const connectForUser = (registration) => {
 
-    const ws_key = `${registration.instanceUrl}:${registration.app_id}:${registration.tag}`;
-    const log = (level, message) => npmlog.log(level, ws_key, message)
+    const log_key = `${registration.instanceUrl}:${registration.appId}:${registration.tag}`;
+    const log = (level, message) => npmlog.log(level, log_key, message)
 
-    const instanceEntry =  getInstanceEntry( registration.instanceUrl);
-    if(instanceEntry){
+    const instanceEntry = getInstanceEntry(registration.instanceUrl);
+    if (instanceEntry) {
         const urlStreamingListenerRegister = instanceEntry.urlStreamingListenerRegister;
-        const appSecret = getAppSecret( instanceEntry,  registration.appId )
-        if( urlStreamingListenerRegister && appSecret ){
+        const appSecret = getAppSecret(instanceEntry, registration.appId)
+        if (urlStreamingListenerRegister && appSecret) {
             // streaming-listener に登録を出す
-            axios.post( urlStreamingListenerRegister, {
+            axios.post(urlStreamingListenerRegister, {
                 instance_url: registration.instanceUrl,
                 tag: registration.tag,
                 app_id: registration.appId,
@@ -150,20 +150,20 @@ const connectForUser = (registration) => {
         }
     }
 }
- 
+
 
 const disconnectForUser = (registration) => {
 
-    const ws_key = `${registration.instanceUrl}:${registration.app_id}:${registration.tag}`;
-    const log = (level, message) => npmlog.log(level, ws_key, message)
-    
-    const instanceEntry =  getInstanceEntry( registration.instanceUrl);
-    if(instanceEntry){
+    const log_key = `${registration.instanceUrl}:${registration.appId}:${registration.tag}`;
+    const log = (level, message) => npmlog.log(level, log_key, message)
+
+    const instanceEntry = getInstanceEntry(registration.instanceUrl);
+    if (instanceEntry) {
         const urlStreamingListenerUnregister = instanceEntry.urlStreamingListenerUnregister;
-        const appSecret = getAppSecret( instanceEntry,  registration.appId )
-        if( urlStreamingListenerUnregister && appSecret ){
+        const appSecret = getAppSecret(instanceEntry, registration.appId)
+        if (urlStreamingListenerUnregister && appSecret) {
             // streaming-listener に登録解除を出す
-            axios.post( urlStreamingListenerUnregister, {
+            axios.post(urlStreamingListenerUnregister, {
                 instance_url: registration.instanceUrl,
                 tag: registration.tag,
                 app_id: registration.appId,
@@ -175,33 +175,34 @@ const disconnectForUser = (registration) => {
             })
         }
     }
-    
-	registration.destroy();
-	log('info', 'Registration destroyed.')
+
+    registration.destroy();
+    log('info', 'Registration destroyed.')
 }
 
-const sendFCM = (registration,payload) => {
+const sendFCM = (registration, payload) => {
 
     const ws_key = `${registration.instanceUrl}:${registration.app_id}:${registration.tag}`;
     const log = (level, message) => npmlog.log(level, ws_key, message)
 
     var serverKey = getServerKey(registration.appId);
-    if( ! serverKey ){
-        log('error','missing server key for app:' +registration.appId);
-        disconnectForUser( registration);
+    if (!serverKey) {
+        log('error', 'missing server key for app:' + registration.appId);
+        disconnectForUser(registration);
         return;
     }
 
     const firebaseMessage = {
         to: registration.deviceToken,
         priority: 'high',
-        data: { notification_id: payload.id }
+        data: {
+            notification_id: payload.id
+        }
     }
 
     axios.post(
         'https://fcm.googleapis.com/fcm/send',
-        JSON.stringify(firebaseMessage),
-        {
+        JSON.stringify(firebaseMessage), {
             headers: {
                 'Authorization': `key=${serverKey}`,
                 'Content-Type': 'application/json'
@@ -214,12 +215,14 @@ const sendFCM = (registration,payload) => {
             return
         }
 
-        response.data.results.forEach( result => {
-            if (result.message_id && result.registration_id ){
+        response.data.results.forEach(result => {
+            if (result.message_id && result.registration_id) {
                 // デバイストークンが更新された
-                registration.update({ deviceToken: result.registration_id }	)
-            } else if ( result.error === 'NotRegistered' ){
-                disconnectForUser( registration);
+                registration.update({
+                    deviceToken: result.registration_id
+                })
+            } else if (result.error === 'NotRegistered') {
+                disconnectForUser(registration);
             }
         })
     }).catch(error => {
@@ -233,71 +236,67 @@ Registration.sync();
 app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
-  res.sendStatus(204)
+    res.sendStatus(204)
 })
 
-app.use('/register',bodyParser.urlencoded({ extended: true }))
+app.use('/register', bodyParser.urlencoded({
+    extended: true
+}))
 
 app.post('/register', (req, res) => {
 
-	const now = (new Date()).getTime();
+    const now = (new Date()).getTime();
 
     var error;
 
     const appId = req.body.app_id;
     error = checkAppId(appId);
-    if( error ){
-        res.send(400,error);
+    if (error) {
+        res.send(400, error);
         return;
     }
-    
+
     const instanceUrl = req.body.instance_url.toLowerCase();
-    error = checkInstanceUrl( instanceUrl , appId)
-    if( error ){
-        res.send(400,error);
+    error = checkInstanceUrl(instanceUrl, appId)
+    if (error) {
+        res.send(400, error);
         return;
     }
-    
+
     const accessToken = req.body.access_token;
     const deviceToken = req.body.device_token;
     const tag = req.body.tag;
-    
-    const options = {
-        where: {
-            instanceUrl: instanceUrl,
-            appId: appId,
-            tag:tag
-        }
-    };
-    
+
     Registration
-        .findOrCreate(options)
-        .then( model => {
+        .findOrCreate({
+            where: {
+                instanceUrl: instanceUrl,
+                appId: appId,
+                tag: tag
+            }
+        })
+        .then(args => {
+            const model = args[0];
+            // const created = args[1];
 
-        if (model ) {
-            npmlog.log('info',"register: " +model)
-            model = model[0];
-            npmlog.log('info',"register: " +model)
-            npmlog.log('info',"register: " +util.inspect(model) )
-            npmlog.log('info',"register: " +model.set)
-            npmlog.log('info',"register: " +model.save)
+            if (model) {
+                model.update({
+                    lastUpdate: now,
+                    deviceToken: deviceToken,
+                    accessToken: accessToken,
+                }).then((ignored) => {
+                    // stream listener への接続を行う
+                    connectForUser(model);
+                });
+            }
+        })
 
-            model.update({
-				lastUpdate: now,
-                deviceToken:deviceToken,
-                accessToken:accessToken,
-            } ).then( (ignored)=>{
-                // stream listener への接続を行う
-                connectForUser( model );
-            });
-		}
-        
-	})
-
-	res.sendStatus(201)
+    res.sendStatus(201)
 })
 
-app.use('/unregister',bodyParser.urlencoded({ extended: true }))
+app.use('/unregister', bodyParser.urlencoded({
+    extended: true
+}))
 
 app.post('/unregister', (req, res) => {
 
@@ -305,56 +304,56 @@ app.post('/unregister', (req, res) => {
 
     const appId = req.body.app_id;
     error = checkAppId(appId);
-    if( error ){
-        res.send(400,error);
+    if (error) {
+        res.send(400, error);
         return;
     }
 
     const instanceUrl = req.body.instance_url.toLowerCase();
-    error = checkInstanceUrl( instanceUrl , appId)
-    if( error ){
-        res.send(400,error);
+    error = checkInstanceUrl(instanceUrl, appId)
+    if (error) {
+        res.send(400, error);
         return;
     }
 
     const tag = req.body.tag;
 
-	Registration.findOne({
-		where: {
-            instanceUrl:instanceUrl,
+    Registration.findOne({
+        where: {
+            instanceUrl: instanceUrl,
             appId: appId,
             tag: tag,
-		}
-	}).then( (registration) => {
-		if (registration ) {
-            npmlog.log('info',"unregister: " +registration)
+        }
+    }).then((registration) => {
+        if (registration) {
+            npmlog.log('info', "unregister: " + registration)
             disconnectForUser(registration)
-		}
-	})
+        }
+    })
 
-	res.sendStatus(201)
+    res.sendStatus(201)
 })
 
-app.use('/callback',bodyParser.json())
+app.use('/callback', bodyParser.json())
 
 app.post('/callback', (req, res) => {
 
     var error;
-    
+
     var json = req.body;
-    
+
 
     const appId = json.appId;
     error = checkAppId(appId);
-    if( error ){
-        res.send(400,error);
+    if (error) {
+        res.send(400, error);
         return;
     }
 
     const instanceUrl = json.instanceUrl;
-    error = checkInstanceUrl( instanceUrl , appId)
-    if( error ){
-        res.send(400,error);
+    error = checkInstanceUrl(instanceUrl, appId)
+    if (error) {
+        res.send(400, error);
         return;
     }
 
@@ -362,15 +361,15 @@ app.post('/callback', (req, res) => {
 
     Registration.findOne({
         where: {
-            instanceUrl:instanceUrl,
+            instanceUrl: instanceUrl,
             appId: appId,
             tag: tag,
         }
-    }).then( (registration) => {
-        if (registration ) {
-            npmlog.log('info',"callback: " +registration)
+    }).then((registration) => {
+        if (registration) {
+            npmlog.log('info', "callback: " + registration)
 
-            sendFCM( registration, Hjson.parse(json.payload) )
+            sendFCM(registration, Hjson.parse(json.payload))
         }
     })
 
@@ -378,33 +377,33 @@ app.post('/callback', (req, res) => {
 })
 
 
-app.get('/counter',(req, res) => {
+app.get('/counter', (req, res) => {
     const log = (level, message) => npmlog.log(level, "counter", message)
 
     const file = 'db/counter.hjson';
-    const file_tmp = file +".tmp";
+    const file_tmp = file + ".tmp";
     var map;
-    try{
-        map = Hjson.parse( fs.readFileSync(file, 'utf8'))
-    }catch(e){
-        log('error',e)
+    try {
+        map = Hjson.parse(fs.readFileSync(file, 'utf8'))
+    } catch (e) {
+        log('error', e)
         map = {};
     }
 
     var count = map.count;
-    if(!count){
+    if (!count) {
         count = 1;
-    }else{
+    } else {
         ++count;
     }
     map.count = count;
-    fs.writeFileSync(file_tmp, Hjson.stringify(map) );
-    fs.renameSync( file_tmp,file)
-    
-    res.send( 200,count);
+    fs.writeFileSync(file_tmp, Hjson.stringify(map));
+    fs.renameSync(file_tmp, file)
+
+    res.send(200, count);
 });
-    
+
 
 app.listen(port, () => {
-  npmlog.log('info', `Listening on port ${port}`)
+    npmlog.log('info', `Listening on port ${port}`)
 })
